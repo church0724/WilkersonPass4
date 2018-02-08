75f209716c79b81dee4d78f2c2564054453f7e4a
church <Church0724@gmail.com>
2018-02-07 20:18:00 -0500

    Update Pass4.java

--- Pass4.java
+++ Pass4.java
@@ -1,5 +1,17 @@
+import java.util.Scanner; //import the Scanner class
+
 public class WilkersonPass4
 {
   static public void main(String [] args)
   {
+   
+    //Create the Scanner object keyboard to read the input
+    Scanner keyboard = new Scanner(System.in);
+    
+    //Create the variables for package prices and extra hours pricing
+    final double packageA = 9.95, packageB = 13.95, packageC = 19.95, packageAExtraHours = 2.00, packageBExtraHours = 1.00;
+     
+    System.out.println("Enter the customer's pacakge (A, B, or C): ");
+    char customerPackage = keyboard.nextChar();
+    
     
